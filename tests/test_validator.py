4e8a67ef30383ae5a051779fcf59ae7441115a8d
sr9000 <no@mail.org>
2025-11-03 00:13:32 +0300

    done with validation

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -24,8 +24,14 @@ from datetime_editor.validator import DateTimeValidator
         ("2025-11-", QValidator.State.Intermediate),
         ("2025-11-0", QValidator.State.Intermediate),
         ("12:", QValidator.State.Intermediate),
+        ("12:3", QValidator.State.Intermediate),
+        ("12:34", QValidator.State.Acceptable),
         ("12:34:", QValidator.State.Intermediate),
+        ("12:34:5", QValidator.State.Intermediate),
+        ("12:34:56", QValidator.State.Acceptable),
         ("12:34:56.", QValidator.State.Intermediate),
+        ("12:34:56.123456", QValidator.State.Acceptable),
+        ("T12:34:56.123456", QValidator.State.Intermediate),
         ("2025-11-02", QValidator.State.Acceptable),
         ("2025-11-02T", QValidator.State.Intermediate),
         ("2025-11-02t1", QValidator.State.Intermediate),
@@ -78,18 +84,47 @@ def test_datetime_validator(text, state):
         ("1*6", "2:3:<4>5", {"12:36", "12:34:56"}),  # also move caret
         (
             "12:34:56*",
-            ".(123)(456)",
+            ".(123)(456)",  # paste at once
             {"12:34:56.123", "12:34:56.123456"},
-        ),  # paste at once
+        ),
+        ("2025-11-02T12:34:56*", "+01:00", {"2025-11-02T12:34:56+01:00"}),  # timezone
+        (
+            "2025-11-02T12:3*",
+            "4:56Z",  # UTC timezone
+            {"2025-11-02T12:34", "2025-11-02T12:34:56", "2025-11-02T12:34:56Z"},
+        ),
+        (
+            "*",
+            "2025-11-02T12:34:56.(123456)Z",  # full typing
+            {
+                "2025-11-02",
+                "2025-11-02T12:34",
+                "2025-11-02T12:34:56",
+                "2025-11-02T12:34:56.123456",
+                "2025-11-02T12:34:56.123456Z",
+            },
+        ),
+        ("2025-1*2T12:34:56", "-0<<1", {"2025-11-02T12:34:56"}),  # insert in middle
+        (
+            "12:34*",
+            ":56.0<<<<<<<<<<T<2025-11-02>>>>>>>>>>>+11:33",  # complex
+            {
+                "12:34:56",
+                "12:34:56.0",
+                "2025-11-02T12:34:56.0",
+                "2025-11-02T12:34:56.0+11:33",
+            },
+        ),
     ],
 )
 def test_like_manual_input(source, pattern, accepted):
     """
     Extended input syntax:
-      - plain chars: typed at caret one by one
-      - '(...)': paste the whole chunk at caret as a single step
+      - '*': initial caret position in source text
+      - plain chars: typed at caret one by one (moving caret forward)
+      - '(...)': paste the whole chunk at caret as a single step (moving caret forward)
       - '<', '>': move caret left and right
-    All non-final texts must be Intermediate; final must be Acceptable and equal to 'result'.
+    All non-final texts must be Intermediate except listed in an `accepted` set.
     """
     # initial text and caret
     caret = source.index("*")
@@ -104,20 +139,25 @@ def test_like_manual_input(source, pattern, accepted):
 
     # tokenize and simulate
     i = 0
-    steps = []  # collect texts after each mutating step
+    res = None
     while i < len(pattern):
-        ch = pattern[i]
-        i += 1
-        match ch:
+        if res == QValidator.State.Acceptable:
+            accepted.discard(text)
+
+        match pattern[
+            (i := i + 1) - 1  # that is an absolute dirty hack for an inline lookahead
+        ]:
             case "(":
                 j = pattern.index(")", i)
                 insert_at(pattern[i:j])
                 i = j + 1
             case "<":
                 caret -= 1
+                continue  # no changes are made
             case ">":
                 caret += 1
-            case _:
+                continue  # no changes are made
+            case ch:
                 insert_at(ch)
 
         res = validator.validate(text, caret)
@@ -131,3 +171,4 @@ def test_like_manual_input(source, pattern, accepted):
             ), f"Expected Intermediate for: {text!r}"
 
     assert text in accepted, f"Result {text!r} is missing in accepted set: {accepted!r}"
+    assert len(accepted) == 1, f"Some extra strings weren't accepted: {accepted!r}"
